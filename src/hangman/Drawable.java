f0a0ec4f77068162b99a58fec41a52512c731f24
Lars Bek <38846235+NineNineFive@users.noreply.github.com>
2018-11-14 13:37:29 +0100

    Update Drawable.java

--- src/hangman/Drawable.java
+++ src/hangman/Drawable.java
@@ -3,12 +3,5 @@ package hangman;
 import javafx.scene.canvas.GraphicsContext;
 
 public interface Drawable {
-
-    /**
-     * Implement this method to get your drawable on the canvas
-     * @param graphicsContext
-     */
     void draw(GraphicsContext graphicsContext);
-
-
 }
